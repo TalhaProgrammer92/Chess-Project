46481349a0d768d4695244b26a12b5b1c433425a
TalhaProgrammer92 <talha.code92@gmail.com>
2025-05-23 12:25:45 +0500

    Update

--- UI/color.py
+++ UI/color.py
@@ -1,21 +1,65 @@
 code: dict = {
-    'foreground': {
-        'bright': {
+    'color': {
+        'foreground': {
+            'bright': {
+                'black': '\033[90m',
+                'red': '\033[91m',
+                'green': '\033[92m',
+                'yellow': '\033[93m',
+                'blue': '\033[94m',
+                'magenta': '\033[95m',
+                'cyan': '\033[96m',
+                'white': '\033[97m'
+            },
 
+            'dark': {
+                'black': '\033[30m',
+                'red': '\033[31m',
+                'green': '\033[32m',
+                'yellow': '\033[33m',
+                'blue': '\033[34m',
+                'magenta': '\033[35m',
+                'cyan': '\033[36m',
+                'white': '\033[37m'
+            }
         },
 
-        'dark': {
+        'background': {
+            'bright': {
+                'black': '\033[100m',
+                'red': '\033[101m',
+                'green': '\033[102m',
+                'yellow': '\033[103m',
+                'blue': '\033[104m',
+                'magenta': '\033[105m',
+                'cyan': '\033[106m',
+                'white': '\033[107m'
+            },
 
+            'dark': {
+                'black': '\033[40m',
+                'red': '\033[41m',
+                'green': '\033[42m',
+                'yellow': '\033[43m',
+                'blue': '\033[44m',
+                'magenta': '\033[45m',
+                'cyan': '\033[46m',
+                'white': '\033[47m'
+            }
         }
     },
 
-    'background': {
-        'bright': {
+    'style': {
+        'bold': '\033[1m',
+        'dim': '\033[2m',
+        'italic': '\033[3m',
+        'under-line': '\033[4m',
+        'blink': '\033[5m',
+        'reverse': '\033[6m',
+        'hidden': '\033[7m'
+    },
 
-        },
-
-        'dark': {
-
-        }
-    }
+    'reset': '\033[0m'
 }
+
+ansi = lambda text, fg, bg, style: fg + bg + style + text + code['reset']
--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+from UI.color import *
 
 if __name__ == '__main__':
-    print("Welcome to Chess Game")
+    print(
+        ansi(
+            'Hello,',
+            code['color']['foreground']['bright']['yellow'],
+            code['color']['background']['dark']['white'],
+            code['style']['bold']
+        ),
+        ansi(
+            'World!',
+            code['color']['foreground']['dark']['red'],
+            code['color']['background']['bright']['cyan'],
+            code['style']['italic'] + code['style']['under-line']
+        )
+    )
