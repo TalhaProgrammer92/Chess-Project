3cbe233e9340e8fe4c09ec6d5cb33c2e4794b90c
TalhaProgrammer92 <talha.code92@gmail.com>
2025-05-23 20:28:48 +0500

    Update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from ui.color import code
 from ui.text import *
 from utils.position import *
+from pieces.unicode import *
 
 if __name__ == '__main__':
     text: Text = Text(
@@ -14,3 +15,11 @@ if __name__ == '__main__':
 
     position1, position2 = Position(row=1, column=2), Position(row=4, column=1)
     print(position1 - position2, position1 == position2)
+
+    print(
+        chess_piece['white']['king'],
+        chess_piece['black']['king'],
+
+        empty_cell['white'],
+        empty_cell['black']
+    )
