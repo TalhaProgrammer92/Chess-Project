4732e840c0d7bf47288c0b7f68e85a7eede40ebe
TalhaProgrammer92 <talha.code92@gmail.com>
2025-05-24 11:10:06 +0500

    Update

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-from pieces.piece import *
 
 if __name__ == '__main__':
-    chess_piece: Pawn = Pawn(_type='white')
-    destination: Position = Position(row=2, column=0)
-    print(chess_piece.is_valid_move(destination))
-    chess_piece.move(destination)
-    print(chess_piece.is_valid_move(destination))
-    print(chess_piece.is_valid_move(destination))
-
-    chess_piece: Queen = chess_piece.promotion('queen')
-    destination = Position(row=1, column=0)
-    print(chess_piece.is_valid_move(destination))
+    pass
