758922ddb770fab42c2d295b90c2187674a73d19
TalhaProgrammer92 <talha.code92@gmail.com>
2025-05-25 09:42:20 +0500

    Update

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from pieces.piece import *
-from board.board import Board
-from utils.position import Position
-from pieces.handler import PieceHandler
+# from pieces.piece import *
+# from board.board import Board
+# from utils.position import Position
+# from pieces.handler import PieceHandler
+from player.player import Player
+
 
 if __name__ == '__main__':
     # chess_piece: Pawn = Pawn(type='black')
@@ -11,9 +13,13 @@ if __name__ == '__main__':
     # chess_piece: Knight = Knight(type='white')
     # print(chess_piece)
 
-    piece_handle: PieceHandler = PieceHandler()
-    piece_handle.reset()
+    # piece_handle: PieceHandler = PieceHandler()
+    # piece_handle.reset()
+    #
+    # board: Board = Board()
+    # board.place_pieces(piece_handle.pieces)
+    # board.display()
 
-    board: Board = Board()
-    board.place_pieces(piece_handle.pieces)
-    board.display()
+    player: Player = Player(name='Talha Ahmad')
+    player.increment_score(1 * 8 + 2 * (3 + 5) + 9)
+    print(player)
