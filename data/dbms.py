f1ab0830c0989982befbcdaa918daff9c15cd96a
TalhaProgrammer92 <talha.code92@gmail.com>
2025-05-31 07:41:38 +0500

    .

--- data/dbms.py
+++ data/dbms.py
@@ -2,13 +2,21 @@ import csv
 from os.path import join
 
 
-# * Read a csv file
+######################
+# Read a csv file
+######################
 class Reader:
 	def __init__(self, **kwargs):
 		self.__path: str = kwargs.get('path', '')
-		self.__file: str = kwargs.get('file', '')
+		self.__file: str = kwargs.get('file', '') + '.csv'
 		self.__f = open(join(self.__path, self.__file), 'r')
 		self.__reader = csv.reader(self.__f, delimiter=kwargs.get('delimiter', ','))
 	
 	def __repr__(self) -> str:
 		return join(self.__path, self.__file)
+
+
+# * Testing
+if '__main__' == __name__:
+	reader: Reader = Reader(path='data', file='file')
+	print(reader)
