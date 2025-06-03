93072345ab01b72bf97ee0b4ded1f065eedf5b6a
TalhaProgrammer92 <talha.code92@gmail.com>
2025-06-03 07:52:13 +0500

    Fixed

--- data/csv_handler.py
+++ data/csv_handler.py
@@ -8,57 +8,57 @@ from logic.game import Game
 # Read a csv file
 ######################
 class Reader:
-	def __init__(self, **kwargs):
-		self.__path: str = join(
-			kwargs.get('path', ''),
-			kwargs.get('file_name', '') + '.csv'
-		)
-		self.__file = open(self.__path, 'r')
-		self.__reader = csv.reader(self.__file, delimiter=kwargs.get('delimiter', ','))
-	
-	# * Getter - CSV Data
-	@property
-	def data(self) -> list[list[str]]:
-		return list(self.__reader)
-	
-	# * Method - Close the opened file
-	def close(self) -> None:
-		self.__file.close()
+    def __init__(self, **kwargs):
+        self.__path: str = join(
+            kwargs.get('path', ''),
+            kwargs.get('file_name', '') + '.csv'
+        )
+        self.__file = open(self.__path, 'r')
+        self.__reader = csv.reader(self.__file, delimiter=kwargs.get('delimiter', ','))
+    
+    # * Getter - CSV Data
+    @property
+    def data(self) -> list[list[str]]:
+        return list(self.__reader)
+    
+    # * Method - Close the opened file
+    def close(self) -> None:
+        self.__file.close()
 
-	# * Method - Representation
-	def __repr__(self) -> str:
-		return self.__path
-	
+    # * Method - Representation
+    def __repr__(self) -> str:
+        return self.__path
+    
 
 ########################
 # Write to csv file
 ########################
 class Writer:
-	def __init__(self, **kwargs):
-		self.__path: str = kwargs.get('path', '')
-		if not exists(self.__path):
-			mkdir(self.__path)
+    def __init__(self, **kwargs):
+        self.__path: str = kwargs.get('path', '')
+        if not exists(self.__path):
+            mkdir(self.__path)
 
-		self.__path: str = join(self.__path, kwargs.get('file_name', '') + '.csv')
-		self.__mode: str = kwargs.get('mode', 'w')	# ! Can only be either 'w' or 'a'
-		self.__file = open(self.__path, self.__mode)
-		self.__writer = csv.writer(self.__file, delimiter=kwargs.get('delimiter', ','))
-	
-	# * Method - Write a single row
-	def write_row(self, data: list) -> None:
-		self.__writer.writerow(data)
-	
-	# * Method - Write multiple rows
-	def write_rows(self, data: list[list]) -> None:
-		self.__writer.writerows(data)
+        self.__path: str = join(self.__path, kwargs.get('file_name', '') + '.csv')
+        self.__mode: str = kwargs.get('mode', 'w')	# ! Can only be either 'w' or 'a'
+        self.__file = open(self.__path, self.__mode)
+        self.__writer = csv.writer(self.__file, delimiter=kwargs.get('delimiter', ','))
+    
+    # * Method - Write a single row
+    def write_row(self, data: list) -> None:
+        self.__writer.writerow(data)
+    
+    # * Method - Write multiple rows
+    def write_rows(self, data: list[list]) -> None:
+        self.__writer.writerows(data)
 
-	# * Method - Close the opened file
-	def close(self) -> None:
-		self.__file.close()
+    # * Method - Close the opened file
+    def close(self) -> None:
+        self.__file.close()
 
-	# * Method - Representation
-	def __repr__(self) -> str:
-		return join(self.__path, self.__file) + f' - {self.__mode}'
+    # * Method - Representation
+    def __repr__(self) -> str:
+        return join(self.__path, self.__file) + f' - {self.__mode}'
 
 
 # * Function - Save a game
@@ -78,26 +78,26 @@ def save_game(game: Game, slot_name: str) -> None:
 
     game_csv.close()
 
-    # # ? Save pieces data
-    # piece_csv: csv.Writer = csv.Writer(
-    #     path=path,
-    #     file_name='Pieces',
-	# 	mode='a'
-    # )
+    # ? Save pieces data
+    piece_csv: csv.Writer = csv.Writer(
+        path=path,
+        file_name='Pieces',
+    	mode='a'
+    )
 
-    # piece_csv.write_row(game.pieces_handler.header)
-	# piece_csv.write_rows(game.pieces_handler.data)
+    piece_csv.write_row(game.pieces_handler.header)
+    piece_csv.write_rows(game.pieces_handler.data)
 
-	# piece_csv.close()
+    piece_csv.close()
 
-	# # ? Save player data
-	# player_csv: csv.Writer = csv.Writer(
-	# 	path=path,
-	# 	file_name='Player',
-	# 	mode='a'
-	# )
+    # ? Save player data
+    player_csv: csv.Writer = csv.Writer(
+    	path=path,
+    	file_name='Player',
+    	mode='a'
+    )
 
-	# player_csv.write_row(game.players[0].header)
-	# player_csv.write_rows([[game.players[i].data] for i in range(len(game.players))])
+    player_csv.write_row(game.players[0].header)
+    player_csv.write_rows([[game.players[i].data] for i in range(len(game.players))])
 
-	# player_csv.close()
+    player_csv.close()
